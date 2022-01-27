1930597e2ed03dacfcb3b165ff62daff985908ab
manimovassagh <mani.mgh@gmail.com>
2022-01-27 20:02:05 +0100

    Secons Commit

--- main.js
+++ main.js
@@ -1,6 +1,26 @@
-class Block{
-  constructor(index,timeStamp,data,previousHash=""){
+const SHA256 = require("crypto-js/sha256");
+
+class Block {
+  constructor(index, timeStamp, data, previousHash = "") {
+    this.index = index;
+    this.timeStamp = timeStamp;
+    this.data = data;
+    this.previousHash = previousHash;
+    this.hash = "";
+  }
+
+  calculateHash() {
+    return SHA256(
+      this.index +
+        this.previousHash +
+        this.timeStamp +
+        JSON.stringify(this.data)
+    ).toString();
+  }
 }
 
-
+class BlockChain {
+  constructor() {
+    this.chain = [];
+  }
 }
